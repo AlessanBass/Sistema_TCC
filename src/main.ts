e3d0b42bec313a660a4caf2b454eef7015dfb554
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2024-03-19 08:56:17 -0300

    add: configuration for views and handlebars

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Configuração para visualizações (views)
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+  // Configuração para arquivos estáticos
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
   await app.listen(3000);
 }
 bootstrap();
