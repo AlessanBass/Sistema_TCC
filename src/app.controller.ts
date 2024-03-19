7175b221de131be1085413af5e13ddd7ffedabc2
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2024-03-19 01:18:04 -0300

    feature: Excel reading finished

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { join } from 'path';
+import { writeFile } from 'fs/promises';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello() {
     return this.appService.getHello();
+  }
+
+  @UseInterceptors(FileInterceptor('file'))
+  @Post()
+  async upload(@UploadedFile() file: Express.Multer.File){
+     const path = join(__dirname, '..', 'storage', file.originalname);
+     writeFile(path, file.buffer);
+     this.appService.readExecel(file, path);
+
   }
 }
