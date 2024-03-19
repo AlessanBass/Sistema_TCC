689697282a1b65ff72ea20cc33fdc2d650a788b9
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2024-03-19 15:22:45 -0300

    Add: Finalizing Handlebars configuration for using partials

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import exphbs = require('express-handlebars');
+
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -9,8 +11,22 @@ async function bootstrap() {
   // Configuração para visualizações (views)
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
+
   // Configuração para arquivos estáticos
   app.useStaticAssets(join(__dirname, '..', 'public'));
+
+  // Configuração para Handlebars partials
+  const hbs = exphbs.create({
+    extname: '.hbs',
+    defaultLayout: 'index',
+    layoutsDir: join(__dirname, '..', 'views'),
+    partialsDir: [
+      join(__dirname, '..', 'views/partials'),
+    ]
+  });
+
+  app.engine('hbs', hbs.engine);
+
 
   await app.listen(3000);
 }
