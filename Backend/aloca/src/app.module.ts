f3c3408a15f35d8ccf72b9d517079a37daff6158
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2024-04-18 18:26:33 -0300

    Criação MOdule Prisma

--- Backend/aloca/src/app.module.ts
+++ Backend/aloca/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProfessorModule } from './professor/professor.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ProfessorModule],
+  imports: [ProfessorModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
