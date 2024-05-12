c89575221d29f598429c64aeeea7e0dfcd8eebdc
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2024-05-12 00:35:38 -0300

    CRUD entidade área Backend

--- Backend/aloca/src/area/area.controller.ts
+++ Backend/aloca/src/area/area.controller.ts
@@ -8,27 +8,27 @@ export class AreaController {
   constructor(private readonly areaService: AreaService) {}
 
   @Post()
-  create(@Body() createAreaDto: CreateAreaDto) {
+  async create(@Body() createAreaDto: CreateAreaDto) {
     return this.areaService.create(createAreaDto);
   }
 
   @Get()
-  findAll() {
+  async findAll() {
     return this.areaService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  async findOne(@Param('id') id: string) {
     return this.areaService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAreaDto: UpdateAreaDto) {
+  async update(@Param('id') id: string, @Body() updateAreaDto: UpdateAreaDto) {
     return this.areaService.update(+id, updateAreaDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  async remove(@Param('id') id: string) {
     return this.areaService.remove(+id);
   }
 }
