67b98c1c9b8fba25bb33ee4f5801ba3820eff86d
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2024-05-21 14:44:40 -0300

    Verificações planilha e geração de erros para planilha sem período

--- Backend/aloca/src/envio_oferta/envio_oferta.service.ts
+++ Backend/aloca/src/envio_oferta/envio_oferta.service.ts
@@ -26,6 +26,13 @@ interface Disicplina {
   area_id_area: number;
 }
 
+interface Erro {
+  linha: number,
+  mensagem: string,
+  valor_celular: string | number | null | undefined,
+  tipo_esperado: string,
+  detalhe: string | null
+}
 
 @Injectable()
 export class EnvioOfertaService {
@@ -81,86 +88,41 @@ export class EnvioOfertaService {
       data.push(rowData);
     });
 
-    await this.excelDataProcessing(data);
+    let erros = await this.excelDataProcessing(data);
     /* Jogar essa isso de apagar em uma função */
     try {
       await unlink(path);
-      console.log('Arquivo excluído com sucesso');
+  /*     console.log('Arquivo excluído com sucesso'); */
     } catch (error) {
-      console.error('Erro ao excluir o arquivo:', error);
+      /* console.error('Erro ao excluir o arquivo:', error); */
     }
+
+   /*  if (erros.length > 0) {
+      throw new BadRequestException(erros);
+    } */
   }
 
   async excelDataProcessing(data: any[]) {
-    if(data[0].column1.trim().toUpperCase() === "PERÍODO"){
+    let erros = [];
+    if (data[0].column1.trim().toUpperCase() === "PERÍODO") {
       console.log("Entrei aqui");
-    
-    for (let i = 1; i < data.length; i++) {
-      const item = data[i];
-      let area = await this.findArea(item);
-      let curso = await this.findCurso(item);
-      let disciplina = await this.findDisciplina(item, curso.id_curso);
-      let semestre = await this.findSemestre(item); /* Retona o ultimo semestre inserido */
-      /* Aqui eu creio uma oferta */
-      
-      if (disciplina && curso && semestre && area) { 
-        if (item.column6 === undefined) {
-          item.column6 = "SEM TURMA";
-        }
 
-        if (typeof item.column6 === 'number') {
-          item.column6 = item.column6.toString();
-        }
-        if (item.column11 === undefined) {
-          item.column11 = null
-        }
+      for (let i = 1; i < data.length; i++) {
+        const item = data[i];
+        let area = await this.findArea(item);
+        let curso = await this.findCurso(item);
+        let disciplina = await this.findDisciplina(item, curso.id_curso);
+        let semestre = await this.findSemestre(item); /* Retona o ultimo semestre inserido */
+        /* Aqui eu creio uma oferta */
 
-        if (item.column12 === undefined) {
-          item.column12 = null
-        }
-
-        if (typeof item.column11 === 'number') {
-          item.column11 = item.column11.toString();
-        }
-
-        let newOfeta = {
-          turma: item.column6,
-          formandos: item.column11,
-          obs_colegiados: item.column12,
-          disciplina_id_disciplina: disciplina.id_disciplina,
-          semestre_id_semestre: semestre.id_semestre,
-          area_id_area: area.id_area,
-        }
-        let retorno = await this.createOferta(newOfeta);
-      } else { /* Aqui eu crio uma disciplina */
-        /* Falta criar curso, area e semstre, caso não existam ainda */
-        let periodo = item.column1;
-        if (typeof periodo === 'string') {
-          periodo = 100;
-        }
-
-        let newDisciplina = {
-          periodo: periodo,
-          cod: item.column2,
-          nome_disciplina: item.column3,
-          carga_horaria: (+item.column4),
-          qtd_creditos: (+item.column5),
-          curso_id_curso: curso.id_curso,
-          area_id_area: area.id_area,
-        }
-        /* Colocar isso em uma função */
-        let retorno = await this.createDisciplina(newDisciplina);
-        if (!retorno) {
-          console.log(`erro ao salavar a disciplina ${newDisciplina.nome_disciplina}`);
-        } else {
+        if (disciplina && curso && semestre && area) {
           if (item.column6 === undefined) {
-            item.column6 = "SEM TURMA"
+            item.column6 = "SEM TURMA";
           }
 
           if (typeof item.column6 === 'number') {
             item.column6 = item.column6.toString();
           }
-
           if (item.column11 === undefined) {
             item.column11 = null
           }
@@ -177,123 +139,160 @@ export class EnvioOfertaService {
             turma: item.column6,
             formandos: item.column11,
             obs_colegiados: item.column12,
-            disciplina_id_disciplina: retorno.id_disciplina,
+            disciplina_id_disciplina: disciplina.id_disciplina,
             semestre_id_semestre: semestre.id_semestre,
-            area_id_area: retorno.area_id_area,
+            area_id_area: area.id_area,
           }
-          await this.createOferta(newOfeta);
+          let retorno = await this.createOferta(newOfeta);
+        } else { /* Aqui eu crio uma disciplina */
+          /* Falta criar curso, area e semstre, caso não existam ainda */
+          let periodo = item.column1;
+          if (typeof periodo === 'string') {
+            periodo = 100;
+          }
+
+          let newDisciplina = {
+            periodo: periodo,
+            cod: item.column2,
+            nome_disciplina: item.column3,
+            carga_horaria: (+item.column4),
+            qtd_creditos: (+item.column5),
+            curso_id_curso: curso.id_curso,
+            area_id_area: area.id_area,
+          }
+          /* Colocar isso em uma função */
+          let retorno = await this.createDisciplina(newDisciplina);
+          if (!retorno) {
+            console.log(`erro ao salavar a disciplina ${newDisciplina.nome_disciplina}`);
+          } else {
+            if (item.column6 === undefined) {
+              item.column6 = "SEM TURMA"
+            }
+
+            if (typeof item.column6 === 'number') {
+              item.column6 = item.column6.toString();
+            }
+
+            if (item.column11 === undefined) {
+              item.column11 = null
+            }
+
+            if (item.column12 === undefined) {
+              item.column12 = null
+            }
+
+            if (typeof item.column11 === 'number') {
+              item.column11 = item.column11.toString();
+            }
+
+            let newOfeta = {
+              turma: item.column6,
+              formandos: item.column11,
+              obs_colegiados: item.column12,
+              disciplina_id_disciplina: retorno.id_disciplina,
+              semestre_id_semestre: semestre.id_semestre,
+              area_id_area: retorno.area_id_area,
+            }
+            await this.createOferta(newOfeta);
+          }
         }
       }
-    }
-  }else{
-    console.log("Não tem periodo");
-    for (let i = 1; i < data.length; i++) {
-      const item = data[i];
+    } else {
+      /* Tabela sem período */
+      for (let i = 1; i < data.length; i++) {
+        const item = data[i];
+        /* Preciso tratar tbm linhas apenas com dados como total de créditos */
+        if ((item.column1 === undefined || typeof item.column1 === 'object') && (item.column2 === undefined || typeof item.column2 === 'object')) {
+          continue;
+        }
+        /* Verifica disciplinas que estão sem código */
+        if (item.column1 === null || item.column1 === undefined || typeof item.column1 === 'object') {
+          /* Retornoar um erro no array */
+          let linha = i;
+          let newErro: Erro = {
+            linha: linha + 1,
+            mensagem: "Disciplinha de encontra sem o código",
+            valor_celular: "undefined",
+            tipo_esperado: "string",
+            detalhe: "Por favor, forneça um valor"
+          }
+          erros.push(newErro);
+          continue;
+        } /* ccertos: 360, 357, 177 */
 
-       /* Verificar disciplinas que estõa sem código */
-       if(item.column1 === null || item.column1 === undefined ){
-        /* Retornoar um erro no array */
-        console.log(`linha: ${i} Entrei aquie em: ${item.column2}`);
-        continue;
-      }
-
-      console.log(`COD: ${item.column1}`);
-      console.log(`Disciplina: ${item.column2}`);
-      console.log(`Turma: ${item.column5}`);
-      console.log(`Curso: ${item.column7}`);
-      console.log(`Area: ${item.column9}`);
-      console.log(`Qtd_creditos: ${item.column4}`);
-      let area = await this.findArea(item);
-      let curso = await this.findCurso2(item);
-      console.log(`ID CURSO: ${curso.id_curso}`);
-      console.log(`-------------------------------`);
-      let disciplina = await this.findDisciplina2(item, curso.id_curso);
-      let semestre = await this.findSemestre(item); /* Retona o ultimo semestre inserido */
-      
-      if (disciplina && curso && semestre && area) { 
-        console.log("Paseei aqui disciplina existe " + disciplina.nome_disciplina);
-        if (item.column5 === undefined) {
-          item.column5 = "SEM TURMA";
+        /* Verifica se a disciplina tá Qtd_créditos */
+        if (item.column4 === null || item.column4 === undefined || typeof item.column4 === 'object') {
+          let linha = i;
+          let newErro: Erro = {
+            linha: linha + 1,
+            mensagem: "Disciplinha de encontra sem a quantidade de créditos",
+            valor_celular: "undefined",
+            tipo_esperado: "number",
+            detalhe: "Por favor, forneça um valor"
+          }
+          erros.push(newErro);
+          continue;
         }
 
-        if (typeof item.column5 === 'number') {
-          item.column5 = item.column5.toString();
-        }
-        if (item.column10 === undefined) {
-          item.column10 = null
-        }
-
-        if (item.column11 === undefined) {
-          item.column11 = null
-        }
-
-        if (typeof item.column10 === 'number') {
-          item.column10 = item.column10.toString();
-        }
-
-        /* Verifica tipo RichText */
-        if(item.column11 && item.column11.richText){
-           item.column11 = item.column11.richText.map(part => part.text).join('');
-        }
-        
-        let newOfeta = {
-          turma: item.column5,
-          formandos: item.column10,
-          obs_colegiados: item.column11,
-          disciplina_id_disciplina: disciplina.id_disciplina,
-          semestre_id_semestre: semestre.id_semestre,
-          area_id_area: area.id_area,
-        }
-       /*  console.log(newOfeta); */
-       console.log("Passei aqui em oferta");
-        let retorno = await this.createOferta(newOfeta);
-      } else { /* Aqui eu crio uma disciplina */
-        /* Falta criar curso, area e semstre, caso não existam ainda */
-       console.log("Preciso criar a disciplina: " + item.column2);
-        let periodo = 0;
-        if (typeof periodo === 'string') {
-          periodo = 100;
+        /* Verifica a carga horária */
+        if (item.column3 === null || item.column3 === undefined || typeof item.column3 === 'object') {
+          let linha = i;
+          let newErro: Erro = {
+            linha: linha + 1,
+            mensagem: "Disciplinha de encontra sem a carga horária",
+            valor_celular: "undefined",
+            tipo_esperado: "number",
+            detalhe: "Por favor, forneça um valor"
+          }
+          erros.push(newErro);
+          continue;
         }
 
-        if(item.column4 === null || item.column4 === undefined || Number.isNaN(item.column4) || typeof item.column4 === 'object'){
-         /*  console.log('Entrei aqui'); */
-          /* console.log(item.column4); */
-          item.column4 = item.column4.result;
-          console.log(item.column4);
+        /* Verifica se tem curso alocado */
+        if (item.column7 === null || item.column7 === undefined || typeof item.column7 === 'object') {
+          let linha = i;
+          let newErro: Erro = {
+            linha: linha + 1,
+            mensagem: "Coluna Curso de encontra vazia",
+            valor_celular: "undefined",
+            tipo_esperado: "String",
+            detalhe: "Por favor, forneça um valor"
+          }
+          erros.push(newErro);
+          continue;
+        }
+        let area = await this.findArea(item);
+        let curso = await this.findCurso2(item);
+        /* Verifica se o curso existe ou não */
+        if(!curso){
+          let linha = i;
+          let newErro: Erro = {
+            linha: linha + 1,
+            mensagem: "Curso não existe",
+            valor_celular: "undefined",
+            tipo_esperado: "Disciplina",
+            detalhe: "Por favor, forneça um valor"
+          }
+          erros.push(newErro);
+          continue;
         }
 
-        let newDisciplina = {
-          periodo: periodo,
-          cod: item.column1,
-          nome_disciplina: item.column2,
-          carga_horaria: (+item.column3),
-          qtd_creditos: (+item.column4),
-          curso_id_curso: curso.id_curso,
-          area_id_area: area.id_area,
-        }
+        let disciplina = await this.findDisciplina2(item, curso.id_curso);
+        let semestre = await this.findSemestre(item); /* Retona o ultimo semestre inserido */
 
-       /*  console.log(newDisciplina); */
-        /* Colocar isso em uma função */  
-
-        console.log("Paseei aqui antes de criar a disciplina " + item.column2);
-        console.log(newDisciplina);
-        let retorno = await this.createDisciplina(newDisciplina);
-        
-        if (!retorno) {
-          console.log(`erro ao salavar a disciplina ${newDisciplina.nome_disciplina}`);
-        } else {
+        /* Se eu encontrar a disciiplina, crio uma oferta */
+        if (disciplina && curso && semestre && area) {
           if (item.column5 === undefined) {
-            item.column5 = "SEM TURMA"
+            item.column5 = "SEM TURMA";
           }
 
           if (typeof item.column5 === 'number') {
             item.column5 = item.column5.toString();
           }
-          /* formandos */
           if (item.column10 === undefined) {
             item.column10 = null
           }
-          /* Obseservações */
+
           if (item.column11 === undefined) {
             item.column11 = null
           }
@@ -301,25 +300,89 @@ export class EnvioOfertaService {
           if (typeof item.column10 === 'number') {
             item.column10 = item.column10.toString();
           }
-
           /* Verifica tipo RichText */
-        if(item.column11 && item.column11.richText){
-          item.column11 = item.column11.richText.map(part => part.text).join('');
-       }
+          if (item.column11 && item.column11.richText) {
+            item.column11 = item.column11.richText.map(part => part.text).join('');
+          }
 
           let newOfeta = {
             turma: item.column5,
-            formandos: item.column10, 
+            formandos: item.column10,
             obs_colegiados: item.column11,
-            disciplina_id_disciplina: retorno.id_disciplina,
+            disciplina_id_disciplina: disciplina.id_disciplina,
             semestre_id_semestre: semestre.id_semestre,
-            area_id_area: retorno.area_id_area,
+            area_id_area: area.id_area,
           }
+
+          /* Colocar um try catch */
           await this.createOferta(newOfeta);
+        } else { /* Aqui eu crio uma disciplina */
+          let periodo = 0;
+          if (typeof periodo === 'string') {
+            periodo = 100;
+          }
+
+          if (item.column4 === null || item.column4 === undefined || Number.isNaN(item.column4) || typeof item.column4 === 'object') {
+            item.column4 = item.column4.result;
+          }
+
+          let newDisciplina = {
+            periodo: periodo,
+            cod: item.column1,
+            nome_disciplina: item.column2,
+            carga_horaria: (+item.column3),
+            qtd_creditos: (+item.column4),
+            curso_id_curso: curso.id_curso,
+            area_id_area: area.id_area,
+          }
+
+          /* Encaplsular em um try catch */
+          let retorno = await this.createDisciplina(newDisciplina);
+
+          if (!retorno) {
+            console.log(`erro ao salavar a disciplina ${newDisciplina.nome_disciplina}`);
+          } else {
+            if (item.column5 === undefined) {
+              item.column5 = "SEM TURMA"
+            }
+
+            if (typeof item.column5 === 'number') {
+              item.column5 = item.column5.toString();
+            }
+            /* formandos */
+            if (item.column10 === undefined) {
+              item.column10 = null
+            }
+            /* Obseservações */
+            if (item.column11 === undefined) {
+              item.column11 = null
+            }
+
+            if (typeof item.column10 === 'number') {
+              item.column10 = item.column10.toString();
+            }
+
+            /* Verifica tipo RichText */
+            if (item.column11 && item.column11.richText) {
+              item.column11 = item.column11.richText.map(part => part.text).join('');
+            }
+
+            let newOfeta = {
+              turma: item.column5,
+              formandos: item.column10,
+              obs_colegiados: item.column11,
+              disciplina_id_disciplina: retorno.id_disciplina,
+              semestre_id_semestre: semestre.id_semestre,
+              area_id_area: retorno.area_id_area,
+            }
+            /* Encapesulsar em um trycatch */
+            await this.createOferta(newOfeta);
+          }
         }
       }
     }
-    }
+    console.log(erros);
+    return erros;
   }
 
   async createOferta(newOferta: Oferta) {
@@ -356,7 +419,7 @@ export class EnvioOfertaService {
   }
 
   async findDisciplina2(item: any, id_curso: number) {
-    
+
     let disciplina = await this._disciplina.findOneCodAndCurso(item.column1.trim(), item.column2.trim(), id_curso);
     if (disciplina === null || disciplina === undefined) {
       return null;
@@ -369,7 +432,7 @@ export class EnvioOfertaService {
     let cursoExcel = item.column8.split(' ');
     let tamanho = cursoExcel.length;
     if (cursoExcel.length >= 2) {
-      let curso = await this._curso.getContains(cursoExcel[tamanho-1]);
+      let curso = await this._curso.getContains(cursoExcel[tamanho - 1]);
       if (curso.length >= 1) {
         return curso[0];
       }
@@ -385,7 +448,7 @@ export class EnvioOfertaService {
     let cursoExcel = item.column7.split(' ');
     let tamanho = cursoExcel.length;
     if (cursoExcel.length >= 2) {
-      let curso = await this._curso.getContains(cursoExcel[tamanho-1]);
+      let curso = await this._curso.getContains(cursoExcel[tamanho - 1]);
       if (curso.length >= 1) {
         return curso[0];
       }
@@ -396,7 +459,7 @@ export class EnvioOfertaService {
       }
     }
   }
-/* Reaproveitado */
+  /* Reaproveitado */
   async findSemestre(item: any) {
     let semestre = await this._semestre.findLatest();
     if (semestre == null || semestre === undefined) {
@@ -405,7 +468,7 @@ export class EnvioOfertaService {
       return semestre;
     }
   }
-/* Reaproveitado */
+  /* Reaproveitado */
   async findArea(item: any) {
     let areaExcel = item.column9.split(' ');
     if (areaExcel.length >= 2) {
