e9166c1346fe991f0bd58bdb4eae4ddbfa9b174b
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2024-05-21 15:58:48 -0300

    Verificações planilha e geração de erros para planilha com período

--- Backend/aloca/src/envio_oferta/envio_oferta.service.ts
+++ Backend/aloca/src/envio_oferta/envio_oferta.service.ts
@@ -92,29 +92,101 @@ export class EnvioOfertaService {
     /* Jogar essa isso de apagar em uma função */
     try {
       await unlink(path);
-  /*     console.log('Arquivo excluído com sucesso'); */
+      /*     console.log('Arquivo excluído com sucesso'); */
     } catch (error) {
       /* console.error('Erro ao excluir o arquivo:', error); */
     }
 
-   /*  if (erros.length > 0) {
-      throw new BadRequestException(erros);
-    } */
+    /*  if (erros.length > 0) {
+       throw new BadRequestException(erros);
+     } */
   }
 
   async excelDataProcessing(data: any[]) {
     let erros = [];
     if (data[0].column1.trim().toUpperCase() === "PERÍODO") {
-      console.log("Entrei aqui");
-
       for (let i = 1; i < data.length; i++) {
         const item = data[i];
+        /* Preciso tratar tbm linhas apenas com dados como total de créditos */
+        if ((item.column2 === undefined || typeof item.column2 === 'object') && (item.column3 === undefined || typeof item.column3 === 'object')) {
+          continue;
+        }
+        /* Verifica disciplinas que estão sem código */
+        if (item.column2 === null || item.column2 === undefined || typeof item.column2 === 'object') {
+          /* Retornoar um erro no array */
+          let linha = i;
+          let newErro: Erro = {
+            linha: linha + 1,
+            mensagem: "Disciplinha de encontra sem o código",
+            valor_celular: "undefined",
+            tipo_esperado: "string",
+            detalhe: "Por favor, forneça um valor"
+          }
+          erros.push(newErro);
+          continue;
+        } /* ccertos: 360, 357, 177 */
+
+        /* Verifica se a disciplina tá Qtd_créditos */
+        if (item.column5 === null || item.column5 === undefined || typeof item.column5 === 'object') {
+          let linha = i;
+          let newErro: Erro = {
+            linha: linha + 1,
+            mensagem: "Disciplinha de encontra sem a quantidade de créditos",
+            valor_celular: "undefined",
+            tipo_esperado: "number",
+            detalhe: "Por favor, forneça um valor"
+          }
+          erros.push(newErro);
+          continue;
+        }
+
+        /* Verifica a carga horária */
+        if (item.column4 === null || item.column4 === undefined || typeof item.column4 === 'object') {
+          let linha = i;
+          let newErro: Erro = {
+            linha: linha + 1,
+            mensagem: "Disciplinha de encontra sem a carga horária",
+            valor_celular: "undefined",
+            tipo_esperado: "number",
+            detalhe: "Por favor, forneça um valor"
+          }
+          erros.push(newErro);
+          continue;
+        }
+
+        /* Verifica se tem curso alocado */ 
+        if (item.column8 === null || item.column8 === undefined || typeof item.column8 === 'object') {
+          let linha = i;
+          let newErro: Erro = {
+            linha: linha + 1,
+            mensagem: "Coluna Curso de encontra vazia",
+            valor_celular: "undefined",
+            tipo_esperado: "String",
+            detalhe: "Por favor, forneça um valor"
+          }
+          erros.push(newErro);
+          continue;
+        }
+
         let area = await this.findArea(item);
         let curso = await this.findCurso(item);
+         /* Verifica se o curso existe ou não */
+        if (!curso) {
+          let linha = i;
+          let newErro: Erro = {
+            linha: linha + 1,
+            mensagem: "Curso não existe",
+            valor_celular: "undefined",
+            tipo_esperado: "Disciplina",
+            detalhe: "Por favor, forneça um valor"
+          }
+          erros.push(newErro);
+          continue;
+        }
         let disciplina = await this.findDisciplina(item, curso.id_curso);
         let semestre = await this.findSemestre(item); /* Retona o ultimo semestre inserido */
+        
         /* Aqui eu creio uma oferta */
-
         if (disciplina && curso && semestre && area) {
           if (item.column6 === undefined) {
             item.column6 = "SEM TURMA";
@@ -143,7 +215,8 @@ export class EnvioOfertaService {
             semestre_id_semestre: semestre.id_semestre,
             area_id_area: area.id_area,
           }
-          let retorno = await this.createOferta(newOfeta);
+          /* Encapsular em um try catch */
+          await this.createOferta(newOfeta);
         } else { /* Aqui eu crio uma disciplina */
           /* Falta criar curso, area e semstre, caso não existam ainda */
           let periodo = item.column1;
@@ -183,6 +256,11 @@ export class EnvioOfertaService {
 
             if (typeof item.column11 === 'number') {
               item.column11 = item.column11.toString();
+            }
+
+            /* Verifica tipo RichText */
+            if (item.column12 && item.column12.richText) {
+              item.column12 = item.column12.richText.map(part => part.text).join('');
             }
 
             let newOfeta = {
@@ -264,7 +342,7 @@ export class EnvioOfertaService {
         let area = await this.findArea(item);
         let curso = await this.findCurso2(item);
         /* Verifica se o curso existe ou não */
-        if(!curso){
+        if (!curso) {
           let linha = i;
           let newErro: Erro = {
             linha: linha + 1,
