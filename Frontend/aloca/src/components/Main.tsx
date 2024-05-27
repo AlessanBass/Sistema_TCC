36131e680bae7553ddf5c99e75f7144bd9b6e64e
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2024-05-27 14:31:31 -0300

    Alocação de um iframe com a localização

--- Frontend/aloca/src/components/Main.tsx
+++ Frontend/aloca/src/components/Main.tsx
@@ -15,6 +15,15 @@ export default function Main(){
                 <Card link="http://localhost:3001/semestres" icon="fa-regular fa-calendar-days" title="Gestão de Semestres" description="Visualizar, Editar, Inserir e Deletar"/>
                 <Card link="http://localhost:3001/areas" icon="fa-solid fa-globe" title="Gestão de Áreas" description="Visualizar, Editar, Inserir e Deletar"/>
             </main>
+            {/* <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3857.507267176079!2d-39.17595732572343!3d-14.796761599180662!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x739a9cffe548e19%3A0xe91ca23dc7294b8!2sUniversidade%20Estadual%20de%20Santa%20Cruz%20%E2%80%93%20UESC!5e0!3m2!1spt-BR!2sbr!4v1716830823877!5m2!1spt-BR!2sbr"
+            width="600"
+            height="450"
+            style={{ border: 0 }}
+            allowFullScreen={true}
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+        ></iframe> */}
         </div>
     );
 }
